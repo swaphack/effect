11aa490e52290fad538484bca5395f9b63ea462b
swaphack <809406730@qq.com>
2019-10-24 18:07:04 +0800

    add sever code

---
+++ Assets/Editor/Tools/Server/GameSever.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Foundation.Net;
+using UnityEngine;
+
+namespace Assets.Editor.Tools.Server
+{
+    public class GameSever
+    {
+        private NetServer _sever;
+        private Dictionary<NetClient, Messages> _messages;
+
+        public GameSever()
+        {
+            _sever = new NetServer();
+            _messages = new Dictionary<NetClient, Messages>();
+        }
+
+        public void SetEndPoint(string address, int port)
+        {
+            _sever.SetServerAddress(address, port);
+        }
+
+        public void Start()
+        {
+            _sever.StartListen();
+            _sever.Process();
+        }
+    }
+}
+
+
